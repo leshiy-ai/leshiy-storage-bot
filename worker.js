ef3ee642ab8c3333189e25701a0fcfe8699b3ae9
leshiyalex <leshiyalex@gmail.com>
2026-05-10 15:41:49 +0000

    перенастройка интерфейса - хиадер

--- worker.js
+++ worker.js
@@ -4181,13 +4181,13 @@ function renderVKMiniAppHTML(params, userData, isAdmin, countUser, env) {
               '<div style="font-size: 13px; margin-top: 4px; opacity: 0.8;">📂 ' + lang.folder + ': ' + (isConn ? '<b>' + (data.currentFolder || '') + '</b>' : '—') + '</div>' +
 
           // 2. Добавляем квоту только если подключено
-          (isConn ? (
+          (isConn ?
               '<div class="quota-card" style="margin-top: 10px;">' + 
                 '<div style="font-size:14px; margin-bottom:4px; opacity:0.8;">☁️ Свободное место</div>' + 
                 '<div class="progress-bg"><div id="quotaBar" class="progress-fill"></div></div>' + 
                 '<div id="quotaText" style="font-size:11px; color: #818c99;">Загрузка данных...</div>' + 
               '</div>'
-          ) : '') + 
+          : '') + 
           
           '</div>'; // Закрываем основной контейнер здесь одной точкой с запятой
       }
