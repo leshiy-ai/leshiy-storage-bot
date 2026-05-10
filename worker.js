ea91381e8105bc0a6747a7702ce6c347e60a6810
leshiyalex <leshiyalex@gmail.com>
2026-05-10 18:10:51 +0000

    замена кавычек в функции renderCommands

--- worker.js
+++ worker.js
@@ -4223,7 +4223,7 @@ function renderVKMiniAppHTML(params, userData, isAdmin, countUser, env) {
       if (adminContainer) {
         var adminHtml = '';
         if (data.isAdmin) {
-          adminHtml += '<span class="blue-link" onclick="togglePanel(' + "'adminPanel'" + ')" style="color:#4bb34b;">/admin</span> — 👑 Меню админа<br>';
+          adminHtml += '<span class="blue-link" onclick="togglePanel(' + '"adminPanel"' + ')" style="color:#4bb34b;">/admin</span> — 👑 Меню админа<br>';
         }
         adminContainer.innerHTML = adminHtml;
       }
@@ -4233,7 +4233,7 @@ function renderVKMiniAppHTML(params, userData, isAdmin, countUser, env) {
       if (!container) return;
       
       var html = '';
-      html += '<span class="blue-link" onclick="togglePanel(' + "'aboutPanel'" + ')">/about</span> — 💬 О приложении<br>';
+      html += '<span class="blue-link" onclick="togglePanel(' + '"aboutPanel"' + ')">/about</span> — 💬 О приложении<br>';
       if (data.isConnected) {
         html += '<span class="blue-link" onclick="openFolderSelector()">/folder</span> — 📂 Выбрать папку для загрузки<br>';
         html += '<span class="blue-link" onclick="shareApp()">/share</span> — 👤 Ссылка для друга<br>';
